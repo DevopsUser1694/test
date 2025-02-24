e730077be1de33fbb5102ee7446c7f93162f53d1
Chandan Kumar <jilimjahim007@outlook.com>
2025-02-24 23:42:22 +0530

    Added crud operation to the functions

--- source/FunctionAppTest/Triggers/ProductTrigger.cs
+++ source/FunctionAppTest/Triggers/ProductTrigger.cs
@@ -1,44 +1,107 @@
+using FunctionAppTest.DAL;
+using FunctionAppTest.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace FunctionAppTest.Triggers
 {
     public class ProductTrigger
     {
         private readonly ILogger<ProductTrigger> _logger;
-        public ProductTrigger(ILogger<ProductTrigger> logger)
+        private readonly AppDbContext _dbContext;
+        public ProductTrigger(AppDbContext dbContext, ILogger<ProductTrigger> logger)
         {
+            _dbContext = dbContext;
             _logger = logger;
         }
 
         [Function("GetProduct")]
-        public IActionResult GetProduct([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+        public async Task<IActionResult> GetProduct([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
             _logger.LogInformation($"{nameof(GetProduct)} trigger function processed a request.");
-            return new OkObjectResult("<Return Product>");
+            var products = await _dbContext.Products.ToListAsync();
+
+            return new OkObjectResult(JsonSerializer.Serialize(products));
         }
 
         [Function("AddProduct")]
-        public IActionResult AddProduct([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        public async Task<IActionResult> AddProduct([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
         {
-            _logger.LogInformation($"{nameof(GetProduct)} trigger function processed a request.");
-            return new OkObjectResult("<Add Product>"); 
+            _logger.LogInformation($"{nameof(AddProduct)} trigger function processed a request.");
+            try
+            {
+                var requestBody = await JsonSerializer.DeserializeAsync<Product>(req.Body);
+
+                if (requestBody == null)
+                {
+                    return new BadRequestObjectResult("Invalid product data.");
+                }
+
+                _dbContext.Products.Add(requestBody);
+                await _dbContext.SaveChangesAsync();
+
+                return new OkObjectResult("Product added successfully!");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception Occured" + ex.Message + " " + ex.InnerException);
+                return new BadRequestObjectResult("Exception Occured.");
+            }
         }
 
         [Function("UpdateProduct")]
-        public IActionResult UpdateProduct([HttpTrigger(AuthorizationLevel.Function, "put")] HttpRequest req)
+        public async Task<IActionResult> UpdateProduct([HttpTrigger(AuthorizationLevel.Function, "put")] HttpRequest req)
         {
             _logger.LogInformation($"{nameof(GetProduct)} trigger function processed a request.");
-            return new OkObjectResult("<Add Product>");
+            try
+            {
+                var product = await JsonSerializer.DeserializeAsync<Product>(req.Body);
+
+                if (product == null)
+                {
+                    return new BadRequestObjectResult("Invalid product data.");
+                }
+
+                _dbContext.Products.Update(product);
+                await _dbContext.SaveChangesAsync();
+
+                return new OkObjectResult("Product Updated successfully!");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception Occured" + ex.Message);
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
 
         [Function("RemoveProduct")]
-        public IActionResult RemoveProduct([HttpTrigger(AuthorizationLevel.Function, "delete")] HttpRequest req)
+        public async Task<IActionResult> RemoveProduct([HttpTrigger(AuthorizationLevel.Function, "delete")] HttpRequest req)
         {
             _logger.LogInformation($"{nameof(GetProduct)} trigger function processed a request.");
-            return new OkObjectResult("<Product Removed>");
+
+            try
+            {
+                var requestBody = await JsonSerializer.DeserializeAsync<Product>(req.Body);
+
+                if (requestBody == null)
+                {
+                    return new BadRequestObjectResult("Invalid product data.");
+                }
+
+                _dbContext.Products.Remove(requestBody);
+                await _dbContext.SaveChangesAsync();
+
+                return new OkObjectResult("Product removed successfully!");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception Occured" + ex.Message);
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
     }
 }
